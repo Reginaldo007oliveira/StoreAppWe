c7448f2ca8f9b7cbf5fa5d21a30989b86e353bcf
Reginaldo <reginaldo007oliveira@gmail.com>
2021-10-10 21:18:11 -0300

    Criação de Types  todos que vc pode precisar em um projeto e criação do component Hello World

--- react/NewHeader.tsx
+++ react/NewHeader.tsx
@@ -1,31 +1,11 @@
-import type { FC } from 'react'
-import React, { useMemo } from 'react'
-import { useProduct } from 'vtex.product-context'
-import { canUseDOM } from 'vtex.render-runtime'
+import React from 'react'
 
-type Props = {
-  label: string
+const NewHeader: StorefrontFunctionComponent = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
 }
 
-const DrawnBadge: FC<Props> = ({ label = 'Your Product!' }) => {
-  const product = useProduct()
-
-  const productId = useMemo(() => product?.product?.productId, [product])
-
-  const drawnProductId =
-    canUseDOM && window.localStorage.getItem('your-product')
-
-  if (!productId || !drawnProductId) {
-    return null
-  }
-
-  return productId === drawnProductId ? (
-    <div className="pa3">
-      <span className="f6 dim br-pill ph3 pv2 mb2 dib white bg-red">
-        {label}
-      </span>
-    </div>
-  ) : null
-}
-
-export default DrawnBadge
+export default NewHeader
